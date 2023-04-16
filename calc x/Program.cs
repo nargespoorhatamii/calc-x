0dd2f2ec39104e42fd290e83ebd02b3797df7f39
nargespoorhatamii <126357653+nargespoorhatamii@users.noreply.github.com>
2023-04-16 10:19:22 +0430

    Add project files.

---
+++ calc x/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace project
+{
+    class test
+    {
+        public static void Main()
+        {
+            Console.WriteLine("inter a number");
+            Double x = Convert.ToDouble(Console.ReadLine());
+            double show=calc(x);
+            Console.Write(show);
+        }
+        private static Double calc(Double x)       
+        {
+            Double Result = Math.Pow(x, 2) + 2 * x - 4;
+            return Result;
+        }
+    }
+  }
+
